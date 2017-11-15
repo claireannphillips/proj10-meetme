f1c18cdc9e41ab84357faf24b2fd0a9c72ea11e4
Claire Phillips <claireannphillips15@gmail.com>
2017-11-15 03:46:31 +0000

    nosetests

---
+++ meetings/nosetest.py
@@ -0,0 +1,11 @@
+from flask_main.py import *
+
+def testing ():
+    
+    #testing for if the event is empty
+    
+    cal_id = 6r4e76kr2b093ag1q5kdom49be
+    start_time = 2017-11-15 at 15:30 to 2017-11-15 at 16:30
+    end_time = 2017-11-15 at 17:30 to 2017-11-15 at 18:30
+    assert(list_events(cal_id, start_time) == end_time)
+    
